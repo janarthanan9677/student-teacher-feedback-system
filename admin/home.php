4506413cdac87eaf337c6444e016eff944bfbd25
janarthanan s <115987891+janarthanan9677@users.noreply.github.com>
2023-10-10 10:16:31 +0530

    Admin

---
+++ admin/home.php
@@ -0,0 +1,79 @@
+<?php include('db_connect.php'); ?>
+<?php 
+function ordinal_suffix1($num){
+    $num = $num % 100; // protect against large numbers
+    if($num < 11 || $num > 13){
+         switch($num % 10){
+            case 1: return $num.'st';
+            case 2: return $num.'nd';
+            case 3: return $num.'rd';
+        }
+    }
+    return $num.'th';
+}
+$astat = array("Not Yet Started","On-going","Closed");
+ ?>
+ <div class="col-12">
+    <div class="card">
+      <div class="card-body">
+        Welcome <?php echo $_SESSION['login_name'] ?>!
+        <br>
+        <div class="col-md-5">
+          <div class="callout callout-info">
+            <h5><b>Academic Year: <?php echo $_SESSION['academic']['year'].' '.(ordinal_suffix1($_SESSION['academic']['semester'])) ?> Semester</b></h5>
+            <h6><b>Evaluation Status: <?php echo $astat[$_SESSION['academic']['status']] ?></b></h6>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
+        <div class="row">
+          <div class="col-12 col-sm-6 col-md-4">
+            <div class="small-box bg-light shadow-sm border">
+              <div class="inner">
+                <h3><?php echo $conn->query("SELECT * FROM faculty_list ")->num_rows; ?></h3>
+
+                <p>Total Faculties</p>
+              </div>
+              <div class="icon">
+                <i class="fa fa-user-friends"></i>
+              </div>
+            </div>
+          </div>
+           <div class="col-12 col-sm-6 col-md-4">
+            <div class="small-box bg-light shadow-sm border">
+              <div class="inner">
+                <h3><?php echo $conn->query("SELECT * FROM student_list")->num_rows; ?></h3>
+
+                <p>Total Students</p>
+              </div>
+              <div class="icon">
+                <i class="fa ion-ios-people-outline"></i>
+              </div>
+            </div>
+          </div>
+           <div class="col-12 col-sm-6 col-md-4">
+            <div class="small-box bg-light shadow-sm border">
+              <div class="inner">
+                <h3><?php echo $conn->query("SELECT * FROM users")->num_rows; ?></h3>
+
+                <p>Total Users</p>
+              </div>
+              <div class="icon">
+                <i class="fa fa-users"></i>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-sm-6 col-md-4">
+            <div class="small-box bg-light shadow-sm border">
+              <div class="inner">
+                <h3><?php echo $conn->query("SELECT * FROM class_list")->num_rows; ?></h3>
+
+                <p>Total Classes</p>
+              </div>
+              <div class="icon">
+                <i class="fa fa-list-alt"></i>
+              </div>
+            </div>
+          </div>
+      </div>
